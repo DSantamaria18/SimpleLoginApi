f809e4cf8aa5b8d6d08e3a0337407ba56e50080a
David Santamaria <dsantamaria_lago@hotmail.com>
2019-02-08 12:23:05 +0100

    Added session class

---
+++ src/main/java/com/stubhub/simplelogin/Session.java
@@ -0,0 +1,28 @@
+package com.stubhub.simplelogin;
+
+
+public class Session {
+    private String username;
+    private String pass;
+
+    public Session(String username, String pass) {
+        this.username = username;
+        this.pass = pass;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
