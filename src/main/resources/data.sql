cb40401ba03e186166405b1ba6321e3c0eaaef76
David Santamaria <dsantamaria_lago@hotmail.com>
2019-02-08 12:24:51 +0100

    Added initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into user (id, username, name, pass, company)
+values(1,'pacoporras@tururu.com', 'PACO PORRAS', '1234', 'PAKIPALLA S.L.');
+
+insert into user (id, username, name, pass, company)
+values(2,'tonygenil@tururu.com', 'TONY GENIL', '1234', 'PAKIPALLA S.L.');
+
+insert into user (id, username, name, pass, company)
+values(3,'julito@churches.com', 'JULIO IGLESIAS', '1234', 'ME VA ME VA S.A.');
+
+insert into user (id, username, name, pass, company)
+values(4,'roncero12@halamadrid.com', 'TOMAS RONCERO', '1234', 'ACS S.A.');
